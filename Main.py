cc67e781d4155d111e6244d665696f93aa5add32
gusta01010 <sonic8783@hotmail.com>
2024-07-25 18:31:52 -0300

    Adicionado coloração do tipo \033 em valores de: Tix, Robux, Status de conta premium e valor monetário

--- Main.py
+++ Main.py
@@ -2,8 +2,7 @@ from ContaRoblox import ContaRoblox
 from NickRoblox import ContaNickname
 from Catalogo import Catalog
 from ContaPremium import Premium
-
-print("Olá! Seja bem-vindo ao ROBlOX!")
+print("\033[34mOlá! Seja bem-vindo ao ROBlOX!\033[0m")
 Nick1 = ContaNickname(input("Para começarmos, digite o seu nickname: "))
 Conta1 = ContaRoblox(Nick1.Get_Nickname())
 print("Boas vindas ao ROBLOX, aqui está a sua conta:")
@@ -14,9 +13,9 @@ while True:
     print("Opções:\n1 - Comprar robux\n2 - Adquirir tickets\n3 - Abrir catálogo\n4 - Tornar-se premium\n5 - Inventário\n6 - Consultar extrato")
     choice = int(input())
     if choice == 1:
-        Conta1.ComprarRobux(int(input("Digite o valor que deseja comprar: ")))
+        Conta1.ComprarRobux(int(input("Digite o valor que deseja comprar: \033[32m")))
     elif choice == 2:
-        Conta1.AdicionarTix(int(input("Digite a quantidade que deseja adicionar: ")))
+        Conta1.AdicionarTix(int(input("Digite a quantidade que deseja adicionar: \033[33m")))
     elif choice == 3:
         Catalog().ComprarChapéu(Conta1) #Chama o ComprarChapéu onde é colocado o objeto que já tem os atributos Conta.robux (no código Catálogo.py) que pode ser alterável
     elif choice == 4:
