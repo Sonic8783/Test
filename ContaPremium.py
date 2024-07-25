6b35ccffd1d361e6f7e9cc6111361e57dc09e6d2
gusta01010 <sonic8783@hotmail.com>
2024-07-25 18:35:36 -0300

    Acrescentamento no prompt e mensagem de erro coloração do tipo \033

--- ContaPremium.py
+++ ContaPremium.py
@@ -2,12 +2,12 @@ class Premium:
     def __init__(self):
         pass
     def PegarPremium(self, Conta):
-        self.select = input("É necessário ter 2.900 robux para tornar-se premium, deseja prosseguir? (s/n): ")
+        self.select = input("É necessário ter \033[32m2.900 robux\033[0m para tornar-se premium, deseja prosseguir? (s/n): ")
         if self.select == 's' and Conta.robux >= 2900 and Conta.IsPremium == False:
             Conta.robux -= 2900
             Conta.IsPremium = True
             print("Premium ativado com sucesso.")
         elif Conta.IsPremium == True:
-            print("Erro: Conta já possui premium.")
+            print("\033[31mErro:\033[0m Conta já possui premium.")
         else:
-            print("Erro: saldo insuficiente.")
+            print("\033[31mErro:\033[0m saldo insuficiente.")
